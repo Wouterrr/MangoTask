f47d7a2031b48bb71345ad865e2acb05473226a1
Wouter Wiegmans <wiegmans@gmail.com>
2015-03-18 22:24:06 +0100

    Worker

--- classes/Model/Queue/Task.php
+++ classes/Model/Queue/Task.php
@@ -4,13 +4,20 @@ class Model_Queue_Task extends Mango {
 	protected $_collection = 'tasks';
 
 	protected $_fields = array(
-		'message'  => array('type' => 'string'),
-		'status'   => array('type' => 'enum', 'values' => array('queued', 'active', 'failed', 'completed')),
-		'created'  => array('type' => 'int'),
-		'updated'  => array('type' => 'int'),
-        'task'     => array('type' => 'string', 'required' =>TRUE),
-        'options'  => array('type' => 'array'),
-        'try'      => array('type' => 'int')
+		// task execution data
+        'status'      => array('type' => 'enum', 'values' => array('queued', 'active', 'failed', 'completed')),
+		'created'     => array('type' => 'int'),
+		'updated'     => array('type' => 'int'),
+        'try'         => array('type' => 'int'),
+        'message'     => array('type' => 'string'),
+
+        // related minion task data
+        'task'        => array('type' => 'string', 'required' =>TRUE),
+        'options'     => array('type' => 'array'),
+
+        // task settings
+        'max_tries'   => array('type' => 'int', 'default' => 2),
+        'keep_failed' => array('type' => 'boolean', 'default' => TRUE)
 	);
 
 	/**
@@ -55,4 +62,75 @@ class Model_Queue_Task extends Mango {
 
 		return parent::update($criteria, $options);
 	}
+
+    public function execute()
+    {
+        // try
+        $options = isset($this->options)
+            ? $this->options->as_array(FALSE)
+            : array();
+
+        $options['task'] = $this->task;
+
+        $success = FALSE;
+
+        try {
+            // create minion task
+            $minion = Minion_Task::factory($options);
+
+            // validate options here, to handle invalid tasks
+            $validation = Validation::factory($minion->get_options());
+            $validation = $minion->build_validation($validation);
+
+            if ( ! $validation->check()) {
+                $errors = $validation->errors(TRUE);
+                $error = array_shift($errors);
+
+                // don't execute task, log validation error
+                Kohana::$log->add(Log::ERROR, 'Task of id: :id and type: :type failed validation: ":msg"', array(
+                    ':id' => (string) $this->_id,
+                    ':type' => (string) $this->task,
+                    ':msg'  => $error
+                ));
+
+                // no need to retry
+                $this->try = $this->max_tries;
+                $this->message = $error;
+            } else {
+                $minion->execute();
+                $success = TRUE;
+            }
+        } catch ( Exception $e) {
+            $error = Kohana_Exception::text($e);
+
+            Kohana::$log->add(Log::ERROR, 'Task :id failed on try: :try with message :msg', array(
+                ':id'  => (string) $this->_id,
+                ':msg' => $error,
+                ':try' => $this->try
+            ));
+
+            $this->message = $error;
+        }
+
+        if ( $success) {
+            // success, delete task
+            $this->delete();
+        } else {
+            $this->status = 'failed';
+
+            if ($this->try < $this->max_tries) {
+                // requeue
+                $this->status = 'queued';
+                $this->update();
+            } else if ( $this->keep_failed) {
+                $this->update();
+            } else {
+                $this->delete();
+            }
+        }
+
+        Kohana::$log->write();
+
+        return $success;
+    }
 }
